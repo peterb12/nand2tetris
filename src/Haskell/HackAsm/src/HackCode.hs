66e6fc6c8032cb928fd693d8686707d13f88848a
Peter Berger <pberger@apple.com>
2019-12-20 16:29:34 -0500

    Emit actual binary strings

--- src/Haskell/HackAsm/src/HackCode.hs
+++ src/Haskell/HackAsm/src/HackCode.hs
@@ -1,5 +1,21 @@
 module HackCode where
 import HackParse
+import Text.Printf
+
+
+encodeCommand :: Command -> String
+encodeCommand (AC ac) = encodeAddress ac
+encodeCommand (CC cc) = encodeOperation cc
+encodeCommand (LC lc) = encodeLabel lc
+
+encodeOperation :: C_Command -> String
+encodeOperation (C dest comp jump) = "111" ++ encodeComp comp ++ encodeDest dest ++ encodeJump jump
+
+encodeAddress :: A_Command -> String
+encodeAddress (AI x) = "0" ++ printf "%015.15b" x
+encodeAddress (AS xs) = undefined
+
+encodeLabel = undefined
 
 encodeDest :: Maybe Dest -> String
 encodeDest Nothing    = "000"
@@ -21,31 +37,31 @@ encodeJump (Just JNE) = "101"
 encodeJump (Just JMP) = "111"
 
 encodeComp :: Comp -> String
-encodeComp Zero      = "101010"
-encodeComp One       = "111111"
-encodeComp NegOne    = "111010"
-encodeComp Dee       = "001100"
-encodeComp Ay        = "110000"
-encodeComp Mm        = "110000"
-encodeComp NotD      = "001101"
-encodeComp NotA      = "110001"
-encodeComp NotM      = "110001"
-encodeComp NegD      = "001111"
-encodeComp NegA      = "110011"
-encodeComp NegM      = "110011"
-encodeComp DPlusOne  = "011111"
-encodeComp APlusOne  = "110111"
-encodeComp MPlusOne  = "110111"
-encodeComp DMinusOne = "001110"
-encodeComp AMinusOne = "110010"
-encodeComp MMinusOne = "110010"
-encodeComp DPlusA    = "000010"
-encodeComp DPlusM    = "000010"
-encodeComp DMinusA   = "010011"
-encodeComp DMinusM   = "010011"
-encodeComp AMinusD   = "000111"
-encodeComp MMinusD   = "000111"
-encodeComp DAndA     = "000000"
-encodeComp DAndM     = "000000"
-encodeComp DOrA      = "010101"
-encodeComp DOrM      = "010101"
+encodeComp Zero      = "0101010"
+encodeComp One       = "0111111"
+encodeComp NegOne    = "0111010"
+encodeComp Dee       = "0001100"
+encodeComp Ay        = "0110000"
+encodeComp Mm        = "1110000"
+encodeComp NotD      = "0001101"
+encodeComp NotA      = "0110001"
+encodeComp NotM      = "1110001"
+encodeComp NegD      = "0001111"
+encodeComp NegA      = "0110011"
+encodeComp NegM      = "1110011"
+encodeComp DPlusOne  = "0011111"
+encodeComp APlusOne  = "0110111"
+encodeComp MPlusOne  = "1110111"
+encodeComp DMinusOne = "0001110"
+encodeComp AMinusOne = "0110010"
+encodeComp MMinusOne = "1110010"
+encodeComp DPlusA    = "0000010"
+encodeComp DPlusM    = "1000010"
+encodeComp DMinusA   = "0010011"
+encodeComp DMinusM   = "1010011"
+encodeComp AMinusD   = "0000111"
+encodeComp MMinusD   = "1000111"
+encodeComp DAndA     = "0000000"
+encodeComp DAndM     = "1000000"
+encodeComp DOrA      = "0010101"
+encodeComp DOrM      = "1010101"
--- src/Haskell/HackAsm/src/Main.hs
+++ src/Haskell/HackAsm/src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import HackParse
+import HackCode
 import qualified Data.Text.IO as T
 import Data.Either
 
@@ -9,7 +10,7 @@ main :: IO ()
 main = do
   asmfile <- T.readFile "/Users/pberger/code/haskell/nand2tetris/projects/06/max/MaxL.asm"
   let theParse = parse parseCommands "" asmfile
-  let result = fromRight [] theParse
+  let result = map encodeCommand (fromRight [] theParse)
   putStrLn $ show result
 
   
